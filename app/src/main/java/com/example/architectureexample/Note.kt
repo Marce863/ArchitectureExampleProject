21d03c1748310fce7936bba292fe76c33c10e3a9
Marcelino Galarza <marcelinogalarza8@gmail.com>
2023-05-10 16:00:11 -0700

    Updating Entity class to follow official doc
    Can be found here
    https://developer.android.com/training/data-storage/room/defining-data#kotlin

--- app/src/main/java/com/example/architectureexample/Note.kt
+++ app/src/main/java/com/example/architectureexample/Note.kt
@@ -7,26 +7,12 @@ import androidx.room.PrimaryKey
 /*
 We call this Note because it represents one note object and a database
 table that'll contain all of these notes
+
+Room annotation, at compile time it'll create all the code to create
+an SQLite table for this object
+Each SQLite table needs a primary key to uniquely identify each entry
  */
-
-// Room annotation, at compile time it'll create all the code to create
-// an SQLite table for this object
 @Entity(tableName = "note_table")
-class Note (var title: String, var description: String, var priority: Int){
-    // Each SQLite table needs a primary key to uniquely identify each entry
-    @PrimaryKey(autoGenerate = true)
-    private var id: Int = 0;
-
-    // Primary constructor parameters are available in property initializers
-    init {
-        // Create member variables for all the fields we want our note to contain
-        // This will be the name of the columns
-        var title: String
-
-        var description: String
-
-        var priority: Int
-    }
-
-    // Kotlin provides getters and setters
-}
+class Note(
+    @PrimaryKey val id: Int, val title: String, val description: String, val priority: Int
+)
